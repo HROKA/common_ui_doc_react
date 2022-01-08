ab54fb96714be6b6799acf79ba9cd8f61ca1db1b
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2022-01-08 12:00:55 +0200

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div>
-    <h1>Common UI toolkit</h1>
-    <nav
-      style={{
-        borderBottom: 'solid 1px',
-        paddingBottom: '1rem',
-      }}
-    >
-      <Link to="/invoices">Invoices</Link>
-      {' '}
-      |
-      {' '}
-      <Link to="/expenses">Expenses</Link>
-    </nav>
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
